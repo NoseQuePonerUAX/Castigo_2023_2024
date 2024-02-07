7163a558af51c9f8c107d6bc6c249c1849b2c2f9
NoseQuePonerUAX <joserafe@myuax.com>
2024-02-07 21:06:54 +0100

    cambio1

--- main.java
+++ main.java
@@ -5,6 +5,10 @@ public class main {
         int horasendia = 24;
         int minutosenhora = 60;
         int segundosenminuto = 60;
-        
+
+        System.out.println("Numero de dias en un ano: " + diasenano);
+        System.out.println("Numero de horas en un dia: " + horasendia);
+        System.out.println("Numero de minutos en una hora: " + minutosenhora);
+        System.out.println("Numero de segundos en un minuto: " + segundosenminuto);
     }
 }
