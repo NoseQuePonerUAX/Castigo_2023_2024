cec6d609938160377279a4e9e345fb08379a225b
NoseQuePonerUAX <joserafe@myuax.com>
2024-02-07 21:02:43 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+
+        int diasenano = 365;
+        int horasendia = 24;
+        int minutosenhora = 60;
+        int segundosenminuto = 60;
+        
+    }
+}
