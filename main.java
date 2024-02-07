5fc114809f717cc7dc3ef24582bdfd113f98be3d
NoseQuePonerUAX <joserafe@myuax.com>
2024-02-07 21:55:05 +0100

    cambio1.2

--- main.java
+++ main.java
@@ -6,9 +6,19 @@ public class main {
         int minutosenhora = 60;
         int segundosenminuto = 60;
 
+        int segundosenano = calcularSegundosenperiodo(1, 0, 0, diasenano, horasendia, minutosenhora, segundosenminuto);
+
         System.out.println("Numero de dias en un ano: " + diasenano);
         System.out.println("Numero de horas en un dia: " + horasendia);
         System.out.println("Numero de minutos en una hora: " + minutosenhora);
         System.out.println("Numero de segundos en un minuto: " + segundosenminuto);
+        System.out.println("El numero de segundos en un año es: " + segundosenano + " segundos.");
+    }
+
+    public static int calcularSegundosenperiodo(int anos, int meses, int dias, int diasenano, int horasendia, int minutosenhora, int segundosenminuto) {
+        int totalDias = anos * diasenano + meses * 30 + dias;
+        int totalHoras = totalDias * horasendia;
+        int totalMinutos = totalHoras * minutosenhora;
+        return totalMinutos * segundosenminuto;
     }
 }
